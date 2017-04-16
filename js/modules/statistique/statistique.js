57c58bfe276fd8a85f92aecb9943c6095afdf1b6
Yamihoshi <a4arkane@gmail.com>
2017-04-16 04:51:36 +0200

    stats chart

--- js/modules/statistique/statistique.js
+++ js/modules/statistique/statistique.js
@@ -1,7 +1,7 @@
 'use strict';
 
 
-var statistique = angular.module('StatistiqueModule', ['ui.router'])
+var statistique = angular.module('StatistiqueModule', ['ui.router', 'nvd3'])
     .factory('statistiqueRepository', ['$http', function ($http) {
         console.log("j'entre");
         return {
@@ -14,7 +14,75 @@ var statistique = angular.module('StatistiqueModule', ['ui.router'])
         }
     }])
     .controller('statsController', [ '$scope', 'questionWithStatistique' , function($scope, questionWithStatistique){
-        console.log(questionWithStatistique);
-    }]);
+        $scope.question = questionWithStatistique.data.question;
+        $scope.propositions = $scope.question.propositions;
+
+        $scope.rep_compare = {
+           chart: {
+                type: 'pieChart',
+                height: 500,
+                x: function(d){return d.label;},
+                y: function(d){return d.value;},
+                showLabels: true,
+                duration: 500,
+                labelThreshold: 0.01,
+                labelSunbeamLayout: true,
+                legend: {
+                    margin: {
+                        top: 5,
+                        right: 35,
+                        bottom: 5,
+                        left: 0
+                    }
+                },
+                color: function(d){return d.color},
+                labelType:"percent"
+            }
+        };
+
+
+        $scope.values_per_proposition = [];
+        var $nb_reponse = [];
+        $nb_reponse[0] = 0;
+        $nb_reponse[1] = 0;
+        for(var index in $scope.propositions)
+        {
+            var $proposition = $scope.propositions[index];
+            var $color = "red";
+
+            if($proposition.verdict==1)
+            {
+                $color="green";
+                $nb_reponse[0]++;
+            }
+            else
+                $nb_reponse[1]++;
+
+            $scope.values_per_proposition.push(
+                {
+                    label: $proposition.title,
+                    value: $proposition.stat.responses_count,
+                    color : $color
+                }
+            );
+        }
+
+
+        $scope.values = [
+            {
+                label: "Bonne réponse",
+                value: $nb_reponse[0],
+                color: "#11400d"
+            },
+            {
+                label: "Mauvaise réponse",
+                value: $nb_reponse[1],
+                color: "#420405"
+            }
+        ];
+
+        $scope.data_to_show = $scope.values;
+
+}]);
 
 
