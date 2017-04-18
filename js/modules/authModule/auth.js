49b1f64437079d7b972ad530216af4308eae0d74
Maxime <maxime.lavaste@gmail.com>
2017-04-18 15:52:19 +0200

    FIX route

--- js/modules/authModule/auth.js
+++ js/modules/authModule/auth.js
@@ -37,7 +37,7 @@ var connexionModule = angular.module('connexionUser', ['ngStorage', 'userAuthMod
         connexionModule.login($scope.username, $scope.password).then(
             function successLogin(response){
                 userAuth.saveToken(response.data.token);
-                $state.go("ues");
+                $state.go("index");
             },
             function errorLogin(error){
                 $scope.authentification.username.$setValidity("size", false);
